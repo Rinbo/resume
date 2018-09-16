1a5106ad79af43780b54966d85510555223b3a9a
rinbo <robin.b@outlook.com>
2018-09-16 15:31:37 +0200

    Initial commit

---
+++ source/javascripts/site.js
@@ -0,0 +1,11 @@
+//= jquery
+//= require vendor/jquery
+//= require vendor/bootstrap.bundle
+//= require vendor/all
+//= require vendor/jquery.easing
+//= require vendor/fontawesome
+//= require vendor/resume
+
+
+
+  
